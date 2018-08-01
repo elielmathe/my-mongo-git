710419e0d9b48d8dd52288b4d296f3417b3aaf70
Eliel <eliel@Eliels-MacBook-Air.local>
2018-08-01 08:10:33 +0200

    Ceci est bon

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
     echo "Very nice";
+    echo "<h1>This is cool</h1>";
     echo "<pre>";
+    echo "\n";
+    
     $db = new Mongo("mongodb://127.0.0.1");
     $cursor = $db -> dbUser -> find;
     
